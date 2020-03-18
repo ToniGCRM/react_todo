c9f8d4141f756dc8531eed57e08f24ab272294b3
Toni Galata <batscott@gmail.com>
2020-03-17 19:08:21 -0600

    final push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import List from './components/todos/List';
+import Form from './components/todos/Form';
+import Footer from './components/todos/Footer';
+
 class App extends Component {
   state = { todos: [
     { id: 1, title: 'Learn Rails', complete: true },
     { id: 2, title: 'Learn React', complete: false },
     { id: 3, title: 'Learn Hooks', complete: false },
-  ]}
+    ],
+    filter: 'All'
+  }
+
+  setFilter = (incomingFilter) => {
+    this.setState({ filter: incomingFilter })
+  }
+
+  visibleTodos = () => {
+    const { todos, filter } = this.state
+    switch(filter) {
+      case 'Active':
+        return todos.filter( t => !t.complete )
+      case 'Complete':
+        return todos.filter( t => t.complete )
+      default:
+        return todos;
+    }
+  }
+
   // renderTodos = () => {
   //   const { todos } = this.state 
   //   return todos.map( todo => {
@@ -14,12 +36,29 @@ class App extends Component {
   //     )
   //   })
   // }
+
+  getUniqId = () => {
+    //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+   }
+
+   addTodo = (todo) => {
+     const { todos } = this.state
+     const newTodo = { id: this.getUniqId(), title: todo.title, complete: false}
+    //  const newTodo = {...todo} will work with a database, spread it out so you don't have to type it
+     this.setState({ todos: [newTodo, ...todos ]})
+   }
+
   render() {
-    const { todos } = this.state
+    const { todos, filter } = this.state
     return(
       <div>
         {/* { this.renderTodos() } */}
-        <List name='Coding todo list' todos={todos} />
+        <List name='Coding todo list' todos={this.visibleTodos()} />
+        <Form addTodo={this.addTodo} />
+        <Footer filter={filter} setFilter={this.setFilter} />
         {/* <List name='Coding todo list' todos /> */}
       </div>
     )
--- src/components/todos/Form.js
+++ src/components/todos/Form.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  state = { title: '' }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = (e) => {
+    //this prevents refresh and other defaults
+    e.preventDefault()
+    //this calls in our props from app.js
+    this.props.addTodo(this.state)
+    //clearing out the form after
+    this.setState({ title: '' })
+  }
+
   render() {
     return (
-      <form>
-        
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          required 
+          placeholder='add todo'
+          name = 'title'
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <button>Submit</button>
       </form>
     )
   }
